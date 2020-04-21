592da8d912d9db1382ea539064ccbe030ea9db15
Johan Félisaz <johan@debian-vm.myhome.net>
2020-04-21 18:43:49 +0200

    added emacsclient stuff

--- init.lisp
+++ init.lisp
@@ -8,3 +8,15 @@
 
 ;; Swap the ctrl and caps-lock keys
 (run-shell-command "setxkbmap -option ctrl:swapcaps")
+
+;; Run emacs as a daemon
+(run-shell-command "emacs --daemon")
+;; Remap C-t C-e and C-t e to emacsclient
+(defcommand emacsclient-launch () ()
+  (run-shell-command "emacsclient -c"))
+
+(define-key *root-map* (kbd "e") "emacsclient-launch")
+(define-key *root-map* (kbd "C-e") "emacsclient-launch")
+
+;; Run emacsclient on startup
+(emacsclient-launch)
